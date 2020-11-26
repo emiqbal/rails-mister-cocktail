fa501f516a99e784ccf807af3c07a9a17f2042d3
iqbal engku <engkuiqbal@outlook.com>
2020-11-26 12:48:25 +0800

    Cocktail New and Create actions done

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -8,8 +8,19 @@ class CocktailsController < ApplicationController
   end
 
   def new
+    @cocktail = Cocktail.new
   end
 
   def create
+    @cocktail = Cocktail.new(name: ct_params[:name])
+    @cocktail.save
+
+    redirect_to cocktail_path(@cocktail)
+  end
+
+  private
+
+  def ct_params
+    params.require(:cocktail).permit(:name)
   end
 end
