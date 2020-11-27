ffeab72ec709d71a4ff85697fdb1abae69e191e3
iqbal engku <engkuiqbal@outlook.com>
2020-11-27 09:02:42 +0800

    Changes to pass Rake

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -15,9 +15,11 @@ class CocktailsController < ApplicationController
 
   def create
     @cocktail = Cocktail.new(name: ct_params[:name])
-    @cocktail.save
-
-    redirect_to cocktail_path(@cocktail)
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
   end
 
   private
